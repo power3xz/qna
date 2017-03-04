20a5ba0e783f91069c8ba9d70f4909a40e52e949
cheolhoon <power3xz@naver.com>
2017-03-05 08:36:22 +0900

    implement compose controller

--- src/app/pages/compose/compose.controller.js
+++ src/app/pages/compose/compose.controller.js
@@ -4,10 +4,35 @@ import  _ from 'lodash/core';
 
 import moment from 'moment';
 
-function ComposeController($log) {
+function ComposeController($log, $state, api) {
   'ngInject';
 
-  $log.debug('Hello from main controller!');
+  const self = this;
+
+  self.question = {
+    title: '',
+    body: '',
+    answers: [{
+      lawyer: '',
+      body: ''
+    }]
+  };
+
+  self.addAnswer = () => self.question.answers.push({ lawyer: '', body: ''});
+  self.submitQuestion = submitQuestion;
+
+  function submitQuestion() {
+    if (isValidQuestion(self.question)) {
+      let q = new api(self.question);
+      api.$save(() => {
+        $state.go('main');
+      });
+    }
+  }
+
+  function isValidQUestion() {
+    return true;
+  }
 }
 
 export default ComposeController;
